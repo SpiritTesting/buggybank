524b84a9ae7047d86ccbf01e100ebc34df91c1b1
Michael Adler <michael.adler@spirit-testing.com>
2019-09-12 19:57:19 +0200

    angular application erstellt

---
+++ src/main/angular/buggybank-frontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'spirit-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'buggybank-frontend';
+}
