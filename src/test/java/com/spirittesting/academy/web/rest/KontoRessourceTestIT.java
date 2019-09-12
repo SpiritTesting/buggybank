de8aafb8fdebd557a46f4a3f0f10ff512d0b058f
Michael Adler <michael.adler@spirit-testing.com>
2019-09-12 16:56:49 +0200

    WebMVC Tests angelegt

---
+++ src/test/java/com/spirittesting/academy/web/rest/KontoRessourceTestIT.java
@@ -0,0 +1,81 @@
+package com.spirittesting.academy.web.rest;
+
+import com.spirittesting.academy.domain.Euro;
+import com.spirittesting.academy.service.KontoService;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+@Sql({"/Kunden.sql", "/Maximalzahlung.sql"})
+@Rollback(false)
+class KontoRessourceTestIT {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Autowired
+    KontoService kontoService;
+
+    @Test
+    void getKonten() throws Exception {
+        mvc.perform(get("/api/konto").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(3)))
+                .andExpect(jsonPath("$[0].kontonummer", is("000")))
+                .andExpect(jsonPath("$[1].kontonummer", is("1")))
+                .andExpect(jsonPath("$[2].kontonummer", is("2")))
+                .andExpect(jsonPath("$[0].betrag", is("EUR 0.00")))
+                .andExpect(jsonPath("$[1].betrag", is("EUR -200.00")))
+                .andExpect(jsonPath("$[2].betrag", is("EUR 200.00")));
+    }
+
+    @Test
+    void getKonto() throws Exception {
+        String date = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        mvc.perform(get("/api/konto/1").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.kontonummer", is("1")))
+                .andExpect(jsonPath("$.betrag", is("EUR -200.00")))
+                .andExpect(jsonPath("$.kreditrahmen", is("EUR 200.00")))
+                .andExpect(jsonPath("$.zahlungen", hasSize(1)))
+                .andExpect(jsonPath("$.zahlungen[0].quelle", is("1")))
+                .andExpect(jsonPath("$.zahlungen[0].ziel", is("2")))
+                .andExpect(jsonPath("$.zahlungen[0].betrag", is("EUR 200.00")))
+                .andExpect(jsonPath("$.zahlungen[0].datum", is(date)));
+    }
+
+    @Test void addKonto() throws Exception {
+        mvc.perform(post("/api/konto").contentType(MediaType.APPLICATION_JSON).content("{\"kundennummer\":\"2\"}"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.kontonummer", is("12340001")))
+                .andExpect(jsonPath("$.betrag", is("EUR 0.00")));
+    }
+
+    @Test
+    void addZahlung() throws Exception {
+        mvc.perform(post("/api/konto/2/1").contentType(MediaType.APPLICATION_JSON).content("{\"betrag\":\"EUR 200" +
+                ".00\"}"))
+                .andExpect(status().isOk());
+        assertEquals(Euro.ZERO, kontoService.getBetrag("1"));
+        assertEquals(Euro.ZERO, kontoService.getBetrag("2"));
+    }
+}
