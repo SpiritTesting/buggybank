16975cca96df69f8518ac902715908b4074fd2f2
Michael Adler <michael.adler@spirit-testing.com>
2019-09-13 10:31:39 +0200

    Angular ist konfiguriert und kommuniziert mit Backend

--- src/main/angular/buggybank-frontend/src/app/app.component.ts
+++ src/main/angular/buggybank-frontend/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Component, OnInit} from '@angular/core';
+import {SERVER_API_URL} from './app.constants';
 
 @Component({
   selector: 'spirit-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'buggybank-frontend';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get(SERVER_API_URL + 'api/kunde', {observe: 'body'}).subscribe(body => console.log(body));
+  }
+
+
 }
--- src/main/angular/buggybank-frontend/src/app/app.module.ts
+++ src/main/angular/buggybank-frontend/src/app/app.module.ts
@@ -1,3 +1,5 @@
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
@@ -12,6 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
