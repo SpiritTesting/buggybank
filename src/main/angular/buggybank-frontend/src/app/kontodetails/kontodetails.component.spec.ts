f75cb73994be21e4c52b7cc2f008cabcb67764fd
Karolina Kopanska <karolina.kopanska@spirit-onside.com>
2022-10-18 13:42:37 +0200

    create unit test for kontodetails component

--- src/main/angular/buggybank-frontend/src/app/kontodetails/kontodetails.component.spec.ts
+++ src/main/angular/buggybank-frontend/src/app/kontodetails/kontodetails.component.spec.ts
@@ -1,25 +1,52 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { KontodetailsComponent } from './kontodetails.component';
+import {DebugElement} from "@angular/core";
+import {RestService} from "../rest.service";
+import {AppModule} from "../app.module";
 
 describe('KontodetailsComponent', () => {
   let component: KontodetailsComponent;
   let fixture: ComponentFixture<KontodetailsComponent>;
-
+  let element: DebugElement;
+  let restService: any;
   beforeEach(async(() => {
+
+    const restServiceSpy = jasmine.createSpyObj('RestService', ['getKonto','putKonto'])
+
     TestBed.configureTestingModule({
-      declarations: [ KontodetailsComponent ]
+      declarations: [ KontodetailsComponent ],
+      providers: [
+        {provide: RestService, useValue: restServiceSpy}
+      ],
+      imports: [
+        AppModule
+      ],
     })
-    .compileComponents();
+    .compileComponents().then(() => {
+
+      fixture = TestBed.createComponent(KontodetailsComponent);
+      component = fixture.componentInstance;
+      element = fixture.debugElement;
+      restService = TestBed.get(RestService);
+    });
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(KontodetailsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should get account number from parameters', () => {
+    pending();
+  });
+  it('should get account from account number ', () => {
+    pending();
+  });
+  it('should set name for account success', () => {
+    pending();
+  });
+  it('should set name for account failure', () => {
+    pending();
   });
 });
